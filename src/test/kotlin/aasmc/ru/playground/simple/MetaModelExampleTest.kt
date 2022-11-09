04411d1fa571df969341ac72d38f02a8763a213d
aleksas <aasmc@yandex.ru>
2022-11-09 16:49:11 +0300

    Add example of getting metamodel in MetaModelExampleTest

---
+++ src/test/kotlin/aasmc/ru/playground/simple/MetaModelExampleTest.kt
@@ -0,0 +1,78 @@
+package aasmc.ru.playground.simple
+
+import aasmc.ru.data.cache.hibernateproviders.HibernateFactory
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import jakarta.persistence.metamodel.*
+import java.util.*
+import kotlin.test.assertFalse
+
+class MetaModelExampleTest {
+    @Test
+    fun metaModelExample() {
+        val entityManagerFactory = HibernateFactory.createTestEntityManagerFactory()
+        val metaModel = entityManagerFactory.metamodel
+        val managedTypes = metaModel.managedTypes
+        // there are 4 entities: Bid, Item, Category and User
+        // and 1 @Embeddable type Address
+        assertEquals(5, managedTypes.size)
+
+        val list = managedTypes.toList()
+        val itemType = list.filter { it.javaType == Item::class.java }
+            .firstNotNullOfOrNull { it } ?: throw NullPointerException()
+
+        val namedAttribute = itemType.getSingularAttribute("name")
+        assertEquals(
+            namedAttribute.javaType,
+            String::class.java
+        )
+
+        assertEquals(
+            namedAttribute.persistentAttributeType,
+            Attribute.PersistentAttributeType.BASIC
+        )
+
+        assertFalse(
+            namedAttribute.isOptional
+        )
+        val auctionEndAttribute =
+            itemType.getSingularAttribute("auctionEnd")
+        assertEquals(
+            auctionEndAttribute.javaType,
+            Date::class.java
+        )
+
+        assertFalse(
+            auctionEndAttribute.isCollection
+        )
+        assertFalse(
+            auctionEndAttribute.isAssociation
+        )
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
